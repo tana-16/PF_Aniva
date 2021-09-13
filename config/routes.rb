87df4c3c39574406f8dcbf9a7367a2ff463016e6
tana-16 <tanahiro1697@gmail.com>
2021-09-13 09:57:13 +0000

    like

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,7 @@ Rails.application.routes.draw do
     patch "users/out" => "users#out"
 
     resources :posts do
+      resource :likes, only: [:create, :destroy]
       resources :comments, only: [:create, :destroy]
     end
     
